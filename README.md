c3be28cfd9a537fe3552958fc2037efd3fdfbca3
serghd <serghd.box@gmail.com>
2026-04-19 20:24:33 +0300

    feat: add raw sql example

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
 # drizzle-postgresql
 An example of the Drizzle ORM + PostgreSQL combination.<br>
 Based on this [instruction](https://orm.drizzle.team/docs/get-started/postgresql-new)
+
+# postgresql
+Create and run docker container:
+```
+docker run --name drizzle-postgres -e POSTGRES_PASSWORD=mypassword -d -p 5432:5432 postgres
+```
+Start stopped container:
+```
+docker start <container_id>
+```
--- src/index.ts
+++ src/index.ts
@@ -1,36 +1,40 @@
 import 'dotenv/config';
 import { drizzle } from 'drizzle-orm/node-postgres';
-import { eq } from 'drizzle-orm';
+import { eq, sql } from 'drizzle-orm';
 import { usersTable } from './db/schema.js';
 
 const db = drizzle(process.env.DATABASE_URL!);
 async function main() {
-  const user: typeof usersTable.$inferInsert = {
-    name: 'John',
-    age: 30,
-    email: 'john@example.com',
-  };
-  await db.insert(usersTable).values(user);
-  console.log('New user created!')
-  const users = await db.select().from(usersTable);
-  console.log('Getting all users from the database: ', users)
-  
-  /*
-  const users: {
-    id: number;
-    name: string;
-    age: number;
-    email: string;
-  }[]
-  */
-  await db
-    .update(usersTable)
-    .set({
-      age: 31,
-    })
-    .where(eq(usersTable.email, user.email));
-  console.log('User info updated!')
-  await db.delete(usersTable).where(eq(usersTable.email, user.email));
-  console.log('User deleted!')
+   // // create item
+   // const user: typeof usersTable.$inferInsert = {
+   //    name: 'John',
+   //    age: 30,
+   //    email: 'john@example.com',
+   // };
+   // await db.insert(usersTable).values(user);
+   // console.log('New user created!');
+
+   // get items
+   const users = await db.select().from(usersTable);
+   console.log('Getting all users from the database: ', users);
+
+   // get items (raw sql)
+   let userId = users[0].id;
+   const statement = sql`select * from ${usersTable} where ${usersTable.id} = ${userId}`;
+   const res = await db.execute(statement);
+   console.log('Getting user from the database (raw Sql): ', res.rows);
+
+   //   // update item
+   //   await db
+   //     .update(usersTable)
+   //     .set({
+   //       age: 31,
+   //     })
+   //     .where(eq(usersTable.email, user.email));
+   //   console.log('User info updated!')
+
+   //   // remove item
+   //   await db.delete(usersTable).where(eq(usersTable.email, user.email));
+   //   console.log('User deleted!')
 }
 main();
